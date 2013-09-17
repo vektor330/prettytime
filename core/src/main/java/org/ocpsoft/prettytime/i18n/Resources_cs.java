9f09d05d2a3bdcad295ae2b8cdc5e959c403c913
Matej Vitásek <vektor330@gmail.com>
2013-09-17 14:21:04 +0200

    Adding a CS and SK localization (future plural forms are known to be wrong for 2, 3 and 4 items).

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_cs.java
@@ -0,0 +1,127 @@
+package com.ecm.realpad.web.utils;
+import java.util.ListResourceBundle;
+
+public class Resources_cs extends ListResourceBundle
+{
+    private static final Object[][] OBJECTS = new Object[][] {
+      { "CenturyPattern", "%n %u" },
+      { "CenturyFuturePrefix", "za " },
+      { "CenturyFutureSuffix", "" },
+      { "CenturyPastPrefix", "před " },
+      { "CenturyPastSuffix", "" },
+      { "CenturyFutureSingularName", "století" },
+      { "CenturyFuturePluralName", "století" },
+      { "CenturyPastSingularName", "stoletím" },
+      { "CenturyPastPluralName", "stoletími" },
+      { "DayPattern", "%n %u" },
+      { "DayFuturePrefix", "za " },
+      { "DayFutureSuffix", "" },
+      { "DayPastPrefix", "před " },
+      { "DayPastSuffix", "" },
+      { "DayFutureSingularName", "den" },
+      { "DayFuturePluralName", "dní" }, // cannot
+      { "DayPastSingularName", "dnem" },
+      { "DayPastPluralName", "dny" },
+      { "DecadePattern", "%n %u" },
+      { "DecadeFuturePrefix", "za " },
+      { "DecadeFutureSuffix", "" },
+      { "DecadePastPrefix", "před " },
+      { "DecadePastSuffix", "" },
+      { "DecadeFutureSingularName", "dekádu" },
+      { "DecadeFuturePluralName", "dekád" }, // cannot
+      { "DecadePastSingularName", "dekádou" },
+      { "DecadePastPluralName", "dekádami" },
+      { "HourPattern", "%n %u" },
+      { "HourFuturePrefix", "za " },
+      { "HourFutureSuffix", "" },
+      { "HourPastPrefix", "pred" },
+      { "HourPastSuffix", "" },
+      { "HourFutureSingularName", "hodinu" },
+      { "HourFuturePluralName", "hodin" }, // cannot
+      { "HourPastSingularName", "hodinou" },
+      { "HourPastPluralName", "hodinami" },
+      { "JustNowPattern", "%u" },
+      { "JustNowFuturePrefix", "" },
+      { "JustNowFutureSuffix", "za chvíli" },
+      { "JustNowPastPrefix", "před chvílí" },
+      { "JustNowPastSuffix", "" },
+      { "JustNowSingularName", "" },
+      { "JustNowPluralName", "" },
+      { "MillenniumPattern", "%n %u" },
+      { "MillenniumFuturePrefix", "za " },
+      { "MillenniumFutureSuffix", "" },
+      { "MillenniumPastPrefix", "před " },
+      { "MillenniumPastSuffix", "" },
+      { "MillenniumFutureSingularName", "milénium" },
+      { "MillenniumFuturePluralName", "milénií" }, // cannot
+      { "MillenniumPastSingularName", "miléniem" },
+      { "MillenniumPastPluralName", "milénii" },
+      { "MillisecondPattern", "%n %u" },
+      { "MillisecondFuturePrefix", "za " },
+      { "MillisecondFutureSuffix", "" },
+      { "MillisecondPastPrefix", "před " },
+      { "MillisecondPastSuffix", "" },
+      { "MillisecondFutureSingularName", "milisekundu" },
+      { "MillisecondFuturePluralName", "milisekund" }, // cannot
+      { "MillisecondPastSingularName", "milisekundou" },
+      { "MillisecondPastPluralName", "milisekundami" },
+      { "MinutePattern", "%n %u" },
+      { "MinuteFuturePrefix", "za " },
+      { "MinuteFutureSuffix", "" },
+      { "MinutePastPrefix", "před " },
+      { "MinutePastSuffix", "" },
+      { "MinuteFutureSingularName", "minutu" },
+      { "MinuteFuturePluralName", "minut" }, // cannot
+      { "MinutePastSingularName", "minutou" },
+      { "MinutePastPluralName", "minutami" },
+      { "MonthPattern", "%n %u" },
+      { "MonthFuturePrefix", "za " },
+      { "MonthFutureSuffix", "" },
+      { "MonthPastPrefix", "před " },
+      { "MonthPastSuffix", "" },
+      { "MonthFutureSingularName", "měsíc" },
+      { "MonthFuturePluralName", "měsíců" }, // cannot
+      { "MonthPastSingularName", "měsícem" },
+      { "MonthPastPluralName", "měsíci" },
+      { "SecondPattern", "%n %u" },
+      { "SecondFuturePrefix", "za " },
+      { "SecondFutureSuffix", "" },
+      { "SecondPastPrefix", "před " },
+      { "SecondPastSuffix", "" },
+      { "SecondFutureSingularName", "sekundu" },
+      { "SecondFuturePluralName", "sekund" }, // cannot
+      { "SecondPastSingularName", "sekundou" },
+      { "SecondPastPluralName", "sekundami" },
+      { "WeekPattern", "%n %u" },
+      { "WeekFuturePrefix", "za " },
+      { "WeekFutureSuffix", "" },
+      { "WeekPastPrefix", "před " },
+      { "WeekPastSuffix", "" },
+      { "WeekFutureSingularName", "týden" },
+      { "WeekFuturePluralName", "týdnů" }, // cannot
+      { "WeekPastSingularName", "týdnem" },
+      { "WeekPastPluralName", "týdny" },
+      { "YearPattern", "%n %u" },
+      { "YearFuturePrefix", "za " },
+      { "YearFutureSuffix", "" },
+      { "YearPastPrefix", "před " },
+      { "YearPastSuffix", "" },
+      { "YearFutureSingularName", "rok" },
+      { "YearFuturePluralName", "let" }, // cannot
+      { "YearPastSingularName", "rokem" },
+      { "YearPastPluralName", "lety" },
+      { "AbstractTimeUnitPattern", "" },
+      { "AbstractTimeUnitFuturePrefix", "" },
+      { "AbstractTimeUnitFutureSuffix", "" },
+      { "AbstractTimeUnitPastPrefix", "" },
+      { "AbstractTimeUnitPastSuffix", "" },
+      { "AbstractTimeUnitSingularName", "" },
+      { "AbstractTimeUnitPluralName", "" } };
+
+    @Override
+    public Object[][] getContents()
+    {
+        return OBJECTS;
+    }
+
+}
---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_sk.java
@@ -0,0 +1,126 @@
+import java.util.ListResourceBundle;
+
+public class Resources_sk extends ListResourceBundle
+{
+    private static final Object[][] OBJECTS = new Object[][] {
+      { "CenturyPattern", "%n %u" },
+      { "CenturyFuturePrefix", "o " },
+      { "CenturyFutureSuffix", "" },
+      { "CenturyPastPrefix", "pred " },
+      { "CenturyPastSuffix", "" },
+      { "CenturyFutureSingularName", "storočie" },
+      { "CenturyFuturePluralName", "storočí" }, // cannot
+      { "CenturyPastSingularName", "storočím" },
+      { "CenturyPastPluralName", "storočiami" },
+      { "DayPattern", "%n %u" },
+      { "DayFuturePrefix", "o " },
+      { "DayFutureSuffix", "" },
+      { "DayPastPrefix", "pred " },
+      { "DayPastSuffix", "" },
+      { "DayFutureSingularName", "deň" },
+      { "DayFuturePluralName", "dní" }, // cannot
+      { "DayPastSingularName", "dňom" },
+      { "DayPastPluralName", "dňami" },
+      { "DecadePattern", "%n %u" },
+      { "DecadeFuturePrefix", "o " },
+      { "DecadeFutureSuffix", "" },
+      { "DecadePastPrefix", "pred " },
+      { "DecadePastSuffix", "" },
+      { "DecadeFutureSingularName", "dekádu" },
+      { "DecadeFuturePluralName", "dekád" }, // cannot
+      { "DecadePastSingularName", "dekádou" },
+      { "DecadePastPluralName", "dekádami" },
+      { "HourPattern", "%n %u" },
+      { "HourFuturePrefix", "o " },
+      { "HourFutureSuffix", "" },
+      { "HourPastPrefix", "pred" },
+      { "HourPastSuffix", "" },
+      { "HourFutureSingularName", "hodinu" },
+      { "HourFuturePluralName", "hodín" }, // cannot
+      { "HourPastSingularName", "hodinou" },
+      { "HourPastPluralName", "hodinami" },
+      { "JustNowPattern", "%u" },
+      { "JustNowFuturePrefix", "" },
+      { "JustNowFutureSuffix", "o chvíľu" },
+      { "JustNowPastPrefix", "pred chvíľou" },
+      { "JustNowPastSuffix", "" },
+      { "JustNowSingularName", "" },
+      { "JustNowPluralName", "" },
+      { "MillenniumPattern", "%n %u" },
+      { "MillenniumFuturePrefix", "o " },
+      { "MillenniumFutureSuffix", "" },
+      { "MillenniumPastPrefix", "pred " },
+      { "MillenniumPastSuffix", "" },
+      { "MillenniumFutureSingularName", "milénium" },
+      { "MillenniumFuturePluralName", "milénií" }, // cannot
+      { "MillenniumPastSingularName", "miléniom" },
+      { "MillenniumPastPluralName", "miléniami" },
+      { "MillisecondPattern", "%n %u" },
+      { "MillisecondFuturePrefix", "o " },
+      { "MillisecondFutureSuffix", "" },
+      { "MillisecondPastPrefix", "pred " },
+      { "MillisecondPastSuffix", "" },
+      { "MillisecondFutureSingularName", "milisekundu" },
+      { "MillisecondFuturePluralName", "milisekúnd" }, // cannot
+      { "MillisecondPastSingularName", "milisekundou" },
+      { "MillisecondPastPluralName", "milisekundami" },
+      { "MinutePattern", "%n %u" },
+      { "MinuteFuturePrefix", "o " },
+      { "MinuteFutureSuffix", "" },
+      { "MinutePastPrefix", "pred " },
+      { "MinutePastSuffix", "" },
+      { "MinuteFutureSingularName", "minútu" },
+      { "MinuteFuturePluralName", "minút" }, // cannot
+      { "MinutePastSingularName", "minútou" },
+      { "MinutePastPluralName", "minútami" },
+      { "MonthPattern", "%n %u" },
+      { "MonthFuturePrefix", "o " },
+      { "MonthFutureSuffix", "" },
+      { "MonthPastPrefix", "pred " },
+      { "MonthPastSuffix", "" },
+      { "MonthFutureSingularName", "mesiac" },
+      { "MonthFuturePluralName", "mesiacov" }, // cannot
+      { "MonthPastSingularName", "mesiacom" },
+      { "MonthPastPluralName", "mesiacmi" },
+      { "SecondPattern", "%n %u" },
+      { "SecondFuturePrefix", "o " },
+      { "SecondFutureSuffix", "" },
+      { "SecondPastPrefix", "pred " },
+      { "SecondPastSuffix", "" },
+      { "SecondFutureSingularName", "sekundu" },
+      { "SecondFuturePluralName", "sekúnd" }, // cannot
+      { "SecondPastSingularName", "sekundou" },
+      { "SecondPastPluralName", "sekundami" },
+      { "WeekPattern", "%n %u" },
+      { "WeekFuturePrefix", "o " },
+      { "WeekFutureSuffix", "" },
+      { "WeekPastPrefix", "pred " },
+      { "WeekPastSuffix", "" },
+      { "WeekFutureSingularName", "týždeň" },
+      { "WeekFuturePluralName", "týždňov" }, // cannot
+      { "WeekPastSingularName", "týždňom" },
+      { "WeekPastPluralName", "týždňami" },
+      { "YearPattern", "%n %u" },
+      { "YearFuturePrefix", "o " },
+      { "YearFutureSuffix", "" },
+      { "YearPastPrefix", "pred " },
+      { "YearPastSuffix", "" },
+      { "YearFutureSingularName", "rok" },
+      { "YearFuturePluralName", "rokov" }, // cannot
+      { "YearPastSingularName", "rokom" },
+      { "YearPastPluralName", "rokmi" },
+      { "AbstractTimeUnitPattern", "" },
+      { "AbstractTimeUnitFuturePrefix", "" },
+      { "AbstractTimeUnitFutureSuffix", "" },
+      { "AbstractTimeUnitPastPrefix", "" },
+      { "AbstractTimeUnitPastSuffix", "" },
+      { "AbstractTimeUnitSingularName", "" },
+      { "AbstractTimeUnitPluralName", "" } };
+
+    @Override
+    public Object[][] getContents()
+    {
+        return OBJECTS;
+    }
+
+}
